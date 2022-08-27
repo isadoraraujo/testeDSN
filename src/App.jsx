329f95c54f9804c1b0e9b5b149e27fb8e0388869
Flavio <fjcf@poli.br>
2022-08-27 12:25:44 -0300

    inicializando projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="App">
+      <h2>uma seleção de produtos</h2>
+      <h1>especial para você</h1>
+      <p>Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
+    </div>
+  )
+}
+
+export default App
